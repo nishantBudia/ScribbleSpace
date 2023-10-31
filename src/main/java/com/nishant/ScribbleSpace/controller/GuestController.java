bc37a7fa719b418081ac1f035ca2d77d1af347a4
NishantBudia <nishantbudia@gmail.com>
2023-10-31 19:57:08 +0530

    test

--- src/main/java/com/nishant/ScribbleSpace/controller/GuestController.java
+++ src/main/java/com/nishant/ScribbleSpace/controller/GuestController.java
@@ -72,9 +72,4 @@ public class GuestController {
             return guestService.getAllCommentsForPostPaginated(id,pageNumber);
         }
     }
-
-    @GetMapping("database-url")
-    public String getDBUrl(){
-        return System.getenv("SCRIBBLE_SPACE_DATABASE_URL");
-    }
 }
